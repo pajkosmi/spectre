aab243ba3e0cc2eb3d59a61a56a09b9f197c0174
pajkosmi <mpajkos@caltech.edu>
2024-04-26 14:57:15 -0700

    Restore proper temperature for CCSN initialize

--- src/PointwiseFunctions/AnalyticData/GrMhd/CcsnCollapse.cpp
+++ src/PointwiseFunctions/AnalyticData/GrMhd/CcsnCollapse.cpp
@@ -520,10 +520,9 @@ tuples::TaggedTuple<hydro::Tags::Temperature<DataType>> CcsnCollapse::variables(
     const gsl::not_null<IntermediateVariables<DataType>*> vars,
     const tnsr::I<DataType, 3>& x,
     tmpl::list<hydro::Tags::Temperature<DataType>> /*meta*/) const {
-    auto temp = make_with_value<Scalar<DataType>>(x, 0.02);
-    return {std::move(temp)};
-  // return {Scalar<DataType>{vars->temperature.value()}};
-  // return {0.02};
+    // auto temp = make_with_value<Scalar<DataType>>(x, 0.02);
+    // return {std::move(temp)};
+  return {Scalar<DataType>{vars->temperature.value()}};
 }
 
 // Specific internal energy
