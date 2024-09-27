f899cad725cf2f1d5dcb622c90211ff954a19ec6
pajkosmi <mpajkos@caltech.edu>
2024-09-27 13:46:19 -0700

    M1 analytic data test stubs

---
+++ tests/Unit/PointwiseFunctions/AnalyticData/RadiationTransport/M1Grey/Test_HomogeneousSphere.cpp
@@ -0,0 +1,26 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include <cmath>
+#include <memory>
+
+#include "Framework/TestCreation.hpp"
+#include "Framework/TestHelpers.hpp"
+#include "Informer/InfoFromBuild.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/AnalyticSolution.hpp"
+#include "Utilities/Serialization/RegisterDerivedClassesWithCharm.hpp"
+
+namespace radiationtransport::AnalyticData {
+namespace {
+
+SPECTRE_TEST_CASE(
+    "Unit.PointwiseFunctions.AnalyticData.RadiationTransport.M1Grey."
+    "HomogeneousSphere",
+    "[Unit][PointwiseFunctions]") {
+        1 == 1;
+    }
+
+}  // namespace
+}  // namespace radiationtransport::AnalyticData
